129a43afd83eb214f3f457fc59d401d2bb56c27e
m0rgause <antoresf1@gmail.com>
2023-10-03 15:27:49 +0700

    ckpn

--- src/app/views/dashboard/ckpn/summary.js
+++ src/app/views/dashboard/ckpn/summary.js
@@ -13,17 +13,56 @@ import {
 import { SearchOutlined, DownloadOutlined } from "@ant-design/icons";
 import { Column } from "@ant-design/plots";
 import dayjs from "dayjs";
+import { post, get } from "../../../functions/helper";
+import QueryString from "qs";
 
 export function SummaryCKPN() {
   const [loading, setLoading] = React.useState(false);
-  const [filterStartDate, setfilterStartDate] = React.useState(dayjs());
+  const [filterStartDate, setfilterStartDate] = React.useState(
+    dayjs().subtract(6, "month")
+  );
   const [filterEndDate, setfilterEndDate] = React.useState(dayjs());
+  const [filterBank, setfilterBank] = React.useState("all");
+
+  const [bank, setBank] = React.useState([]);
+
+  React.useEffect(() => {
+    getBank();
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onFilter = () => {
     setLoading(true);
   };
 
   const isMobile = window.innerWidth <= 768;
+
+  const getData = async () => {
+    let eq = {
+      start: filterStartDate.format("YYYY-MM-DD"),
+      end: filterEndDate.format("YYYY-MM-DD"),
+      range: filterStartDate.diff(filterEndDate, "month"),
+      issuer: filterBank,
+    };
+
+    const { data: data } = await post(
+      "/ckpn/summary",
+      QueryString.stringify(eq)
+    );
+
+    console.log(data);
+  };
+
+  const getBank = async () => {
+    const { data: data } = await get("/issuer/select");
+
+    let item = [{ value: "all", label: "All" }];
+    data.data.rows.forEach((element) => {
+      item.push({ value: element.id, label: element.nama });
+    });
+    setBank(item);
+  };
 
   const dataChart = [
     { bank: "Mandiri", return: 3000 },
@@ -119,11 +158,15 @@ export function SummaryCKPN() {
               </Col>
               <Col span={isMobile ? 24 : 22}>
                 <DatePicker
+                  picker="month"
+                  format={"MM-YYYY"}
                   defaultValue={filterStartDate}
                   onChange={(date) => setfilterStartDate(date)}
                 />{" "}
                 -{" "}
                 <DatePicker
+                  picker="month"
+                  format={"MM-YYYY"}
                   defaultValue={filterEndDate}
                   onChange={(date) => setfilterEndDate(date)}
                 />
@@ -133,12 +176,8 @@ export function SummaryCKPN() {
               </Col>
               <Col span={isMobile ? 24 : 22}>
                 <Select
-                  defaultValue="mandiri"
-                  options={[
-                    { value: "mandiri", label: "Mandiri" },
-                    { value: "bca", label: "BCA" },
-                    { value: "bni", label: "BNI" },
-                  ]}
+                  defaultValue={filterBank}
+                  options={bank}
                   style={{ maxWidth: "300px", width: "100%" }}
                 />
               </Col>
